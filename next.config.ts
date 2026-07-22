bf5be728dc279da36e252078669cf3eaa3dd2179
uwem42020-ux <uwem42020@gmail.com>
2026-07-22 16:54:23 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/marketplace',
+        destination: '/search',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
